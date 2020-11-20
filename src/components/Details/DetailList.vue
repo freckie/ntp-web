b39c0b3fd5e9eabd0d5deec43b43b66ae4d43d7c
freckie <freckie@frec.kr>
2020-11-20 21:53:59 +0900

    Add: details

---
+++ src/components/Details/DetailList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="details-container">
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">
+              크레딧 변동
+            </th>
+            <th class="text-left">
+              날짜
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(item, idx) in details"
+            :key="idx"
+          >
+            <td :style="{ color: item.color }">{{ item.credit }} ￦</td>
+            <td>{{ item.datetime }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detail-list',
+  props: [
+    'payment'
+  ],
+  data: () => {
+    return {
+      details: [
+        {
+          credit: -10000,
+          color: 'red',
+          datetime: '2020-11-20 21:32'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,23 +1,54 @@
 <template>
   <div id="details">
-    <div id="green-container">
+    <h2>결제 내역</h2>
+    <br>
+
+    <div id="detail-container">
+      <v-select
+        :items="cards"
+        label="결제 수단"
+        outlined
+      ></v-select>
+      <br>
+      <detail-list id="detail-list" :payment="payment"></detail-list>
     </div>
   </div>
 </template>
 
 <script>
+import DetailList from '../components/Details/DetailList.vue'
 export default {
-  name: 'Details'
+  name: 'Details',
+  components: {
+    DetailList
+  },
+  data: () => {
+    return {
+      payment: 1,
+      cards: [
+        {
+          text: '#1 Shinhan || Visa',
+          value: '0'
+        },
+        {
+          text: '#2 Shinhan || Visa',
+          value: '1'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 #details {
   height: 100%;
-  background-color: #f4f4f4;
+  background-color: #99F3BD;
 
-  #green-container {
-    background-color: #99F3BD;
+  #detail-container {
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 50px;
   }
 }
 </style>
